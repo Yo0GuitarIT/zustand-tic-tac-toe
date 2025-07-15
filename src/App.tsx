cb314cfcb0dc84e8545be21ed9af2bc803738a60
Yu Ling Chen <chenYuLing@hismax.onmicrosoft.com>
2025-07-15 17:25:28 +0800

    refactor: remove unnecessary state management code and simplify App components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,6 @@
 // import "./App.css";
 import Game from "./components/Game";
 
-import { create } from "zustand";
-
-interface BearState {
-  bears: number;
-  increasePopulation: () => void;
-  removeAllBears: () => void;
-  updateBears: (newBears: number) => void;
-}
-
-const useStore = create<BearState>()((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
-}));
-
-const BearCounter = () => {
-  const bears = useStore((state) => state.bears);
-
-  return <h1>{bears} bears around here...</h1>;
-};
-
-const Controls = () => {
-  const increasePopulation = useStore((state) => state.increasePopulation);
-  return <button onClick={increasePopulation}>one up</button>;
-};
-
 function App() {
   return <Game />;
 }
